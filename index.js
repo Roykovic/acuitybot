1a12c9e633d6fa48777a7788dfc0bd20a1cdadcb
Roykovic <rtersluijsen@acuity.nl>
2017-08-30 15:33:20 +0200

    fix

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ restService.post('/hook', function(req, res) {
 			  console.log('Connected to postgres! Getting schemas...');
 			  client
 				.query('SELECT * FROM salesforce.Contact WHERE name=\'' + fullName + "\';")
-				.then(res => speech = res.rows[0].id)
+				.then(res => console.log(res.rows)
 				.catch(e => console.error(e.stack));
 
 				
