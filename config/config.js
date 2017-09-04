d4b454723942a6269b74496ae49901e052ba668f
Roykovic <rtersluijsen@acuity.nl>
2017-09-04 09:36:29 +0200

    .

---
+++ config/config.js
@@ -0,0 +1,7 @@
+var connectionString = 'postgres://xkmrmtanjzvitd:50a15571798f062acd52e12385a13083eeaa326ca4d562272ef7002fcc2a641e@ec2-54-75-239-190.eu-west-1.compute.amazonaws.com:5432/danmi0s4e2dhn4'
+var user = "xkmrmtanjzvitd"
+var pass = "50a15571798f062acd52e12385a13083eeaa326ca4d562272ef7002fcc2a641e"
+var host = "ec2-54-75-239-190.eu-west-1.compute.amazonaws.com"
+var port = "5432"
+var db = "danmi0s4e2dhn4"
+var connectionString2 = "postrgres://" + user + ":" + pass + "@" + host + ":" + port + "/" + db;
