d9fa359b1c28fb50afbe42c6fd0ca83c304bd8a8
Roykovic <rtersluijsen@acuity.nl>
2017-09-04 09:40:20 +0200

    .

--- index.js
+++ index.js
@@ -17,6 +17,13 @@
 'use strict';
 
 var connectionString = 'postgres://xkmrmtanjzvitd:50a15571798f062acd52e12385a13083eeaa326ca4d562272ef7002fcc2a641e@ec2-54-75-239-190.eu-west-1.compute.amazonaws.com:5432/danmi0s4e2dhn4'
+var user = "xkmrmtanjzvitd"
+var pass = "50a15571798f062acd52e12385a13083eeaa326ca4d562272ef7002fcc2a641e"
+var host = "ec2-54-75-239-190.eu-west-1.compute.amazonaws.com"
+var port = "5432"
+var db = "danmi0s4e2dhn4"
+var connectionString2 = "postgres://" + user + ":" + pass + "@" + host + ":" + port + "/" + db;
+
 var pg = require('pg');
 var speech = 'empty speech';
 
@@ -29,6 +36,7 @@ restService.use(bodyParser.json());
 restService.post('/hook', function(req, res) {
 
     console.log('hook request');
+	console.log(connectionString == connectionString2);
 
 	if(req.body.result.metadata.intentName == "Default Welcome Intent" || req.body.result.action.includes("smalltalk.")){
 		wakeUp(req);
