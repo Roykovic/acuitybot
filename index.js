76b2b8a6c2d237b0a89ca735166141d8b17a7491
Roykovic <rtersluijsen@acuity.nl>
2017-09-05 08:45:44 +0200

    .

--- index.js
+++ index.js
@@ -102,6 +102,9 @@ function checkColumn(column){
 	query("*", "false", function(columns){
 		if(columns){
 			for (var i = 0, len = columns.fields.length; i < len; i++) {
+				console.log(columns.fields[i].name.toLowerCase())
+				console.log(column.toLowerCase)
+				console.log(columns.fields[i].name.toLowerCase() == column.toLowerCase)
 				if(columns.fields[i].name.toLowerCase() == column.toLowerCase){
 					console.log("Column is found in database")
 					return column;
