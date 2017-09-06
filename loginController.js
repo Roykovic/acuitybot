29771a47ec6baaf3cc6f35091bc5df84d4fe8a6d
Roykovic <rtersluijsen@acuity.nl>
2017-09-06 13:52:09 +0200

    .

--- loginController.js
+++ loginController.js
@@ -7,6 +7,7 @@ var db = require('./db');
 exports.loginSucces = function (username, password){
 	db.getUser(username, function(result){
 		if(result){
+			console.log("HOIHOIHOIHOIHOIHOIHOIHOIHOIHOI")
 			if(result.rows[0].pass_c == password){
 				console.log(true);
 			}
