0a3461f6c7e05fd102e5caba7325fe57326c0a2a
Roykovic <rtersluijsen@acuity.nl>
2017-09-06 12:10:09 +0200

    .

--- loginController.js
+++ loginController.js
@@ -2,8 +2,13 @@
 
 var self = this;
 var exports = module.exports = {};
+var db = require('./db');
 
 exports.loginSucces = function (username, password){
+	console.log("Account table:")
+db.query("* FROM salesforce.account --" "idc" function(result){
+	console.log(result.rows[0])
+}
 			if(username == "Roy" && password == "1234" ){
 				return true
 			}
