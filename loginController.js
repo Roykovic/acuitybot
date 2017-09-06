a014d9666cbeaad2bce54d0462932b28d0166479
Roykovic <rtersluijsen@acuity.nl>
2017-09-06 14:07:26 +0200

    .

--- loginController.js
+++ loginController.js
@@ -4,7 +4,7 @@ var self = this;
 var exports = module.exports = {};
 var db = require('./db');
 
-exports.login = function (username, password){
+exports.login = function (username, password, callback){
 	db.getUser(username, function(result){
 		if(result){
 			if(result.rows[0].pass__c == password){
