6ed6db733bfab25363719b817ea292b3d2092f28
Roykovic <rtersluijsen@acuity.nl>
2017-09-07 08:38:03 +0200

    .

--- index.js
+++ index.js
@@ -46,6 +46,8 @@ restService.post('/hook', function(req, res) {
 				speech = "Login failed, please check username and password"	
 			}
 		})
+		console.log("speech")
+		console.log(speech)
 	return returnJson(res, speech)
 
 	}
