6404c364f441dcad288a15bc110e2cc6a31f409e
Roykovic <rtersluijsen@acuity.nl>
2017-09-07 08:39:35 +0200

    .

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ restService.post('/hook', function(req, res) {
 		var user = req.body.result.parameters['Username']
 		var pass = req.body.result.parameters['Password']
 		loginController.login(user, pass, function(succes){
+					console.log("succes")
+		console.log(succes)
 			if(succes){			
 				sessionId = req.body.sessionId;
 				auth = true;		
@@ -46,8 +48,6 @@ restService.post('/hook', function(req, res) {
 				speech = "Login failed, please check username and password"	
 			}
 		})
-		console.log("succes")
-		console.log(succes)
 	return returnJson(res, speech)
 
 	}
