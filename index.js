bdabe84c3aa6e7bdbfbfcf95a515b9b5fd9e0f73
Roykovic <rtersluijsen@acuity.nl>
2017-09-18 11:05:20 +0200

    .

--- index.js
+++ index.js
@@ -150,7 +150,7 @@ function getJSON(options)
 
   res.on("end", function () {
     var body = Buffer.concat(chunks);
-	console.log("HERE")
+	console.log(body)
     console.log(body.toString());
   });
 });
