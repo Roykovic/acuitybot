1394643075a2a311ec91e3263c39d7e80024f441
Roykovic <rtersluijsen@acuity.nl>
2017-09-19 13:40:34 +0200

    .

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ https.get(options, (resp) => {
 		for(var index = 0; index < entries.length; ++index){
 			names += entries[index]['title'][0]['_'];
 			if(index>0){
-				names+=" ,"
+				names=" ," + names
 			}
 		}
 		returnJson("These are you todo's: " + names);
