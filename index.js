0e4bebebfa664c0eaf87ada970d4499f7abf3f60
Roykovic <rtersluijsen@acuity.nl>
2017-09-19 13:55:22 +0200

    .

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ https.get(options, (resp) => {
 			if(index>0){
 				names+= ", "
 			}
-			names += entries[index]['link'][0][0];
+			names += entries[index]['title'][0]['_'];
 		}
 		returnJson("These are you files: " + names);
 	})
