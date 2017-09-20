636c3d189b192f5aff4b738a2f381e8eddf831d5
Roykovic <rtersluijsen@acuity.nl>
2017-09-20 10:12:27 +0200

    .

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ https.get(options, (resp) => {
   text : "PROJECT UPDATE - A new project plan has been published on Box: http://box.com/s/lf5vj. The PM for this project is Mike C. and the Engineering Manager is Jane W.",
   markdown : "**PROJECT UPDATE** A new project plan has been published [on Box](http://box.com/s/lf5vj). The PM for this project is <@personEmail:mike@example.com> and the Engineering Manager is <@personEmail:jane@example.com>.",
 						speech: "Frits",
-						displayText: "Frits",
+						displayText: "**Frits**",
 						source: 'apiai-webhook-sample',
 						followupEvent: {
 							name:null
