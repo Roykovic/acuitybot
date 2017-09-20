f2c4d580816d9bbc20a195c84d59cf5656b91539
Roykovic <rtersluijsen@acuity.nl>
2017-09-20 13:59:07 +0200

    .

--- verseController.js
+++ verseController.js
@@ -41,6 +41,7 @@ https.get(options, (resp) => {
   resp.on('end', () => {
 	var parser = new xml2js.Parser();
 	parser.parseString(data, function (err, HTTPresult){
+				console.log("*****************************************************STATUS CODE*****************************************************")
 				console.log(HTTPresult.statusCode)
 		var entries = HTTPresult['feed']['entry'];
 		var titles = ""
