5105f32a352b80cc71143a8571fe2b8a254b1793
Roykovic <rtersluijsen@acuity.nl>
2017-09-26 14:42:54 +0200

    .

--- verseController.js
+++ verseController.js
@@ -61,6 +61,7 @@ var options = {
 
 http.post('http://apps.ce.collabserv.com/activities/service/atom2/activity?activityUuid=ac7081f8-417c-407c-a3bb-c13ddc541ea8', options, function(res){
 			console.log("************************************Callback************************************")
+			console.log(res)
 res.setEncoding('utf8');
 	res.on('data', function(chunk) {
 			console.log("************************************Data************************************")
