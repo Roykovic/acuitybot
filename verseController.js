12baf0c04e8b2c34cfa484f25ac8dc395b8f9877
Roykovic <rtersluijsen@acuity.nl>
2017-09-26 13:32:27 +0200

    .

--- verseController.js
+++ verseController.js
@@ -58,37 +58,49 @@ var options = {
 		}
 	};
 	
-http.post(options, (resp) => {
-  let data = '';
+	var data = {
+	var body = '<entry xmlns="http://www.w3.org/2005/Atom" xmlns:snx="http://www.ibm.com/xmlns/prod/sn"> <title type="text">TEST22222222222222222</title> <category scheme="http://www.ibm.com/xmlns/prod/sn/type" term="todo" label="To Do"/> <content type="html">          	&lt;p dir="ltr">TEST&lt;/p>      	  </content> <snx:communityUuid/> </entry>'
+};
 
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
-  resp.on('end', () => {
-	var parser = new xml2js.Parser();
-		parser.parseString(data, function (err, HTTPresult){
-		console.log("************************************Data************************************")
-		console.log(data)		
-		console.log("************************************Result************************************")
-		console.log(HTTPresult);
-		var entries = HTTPresult['feed']['entry'];
-		var titles = ""
-		for(var index = 0; index < entries.length; ++index){
-			if(index>0){
-				titles+= ", "
-			}
-			titles += "\n"+entries[index]['title'][0]['_']+"\n";
-			if(type == "files"){
-				var URL = entries[index]['link'][0]['$']['href'].replace(/entry/g, 'media')
-				titles += URL;
-			}
-			
-		}
-		callback("These are you "+type+": " + titles)
-	})
-  });
-
-}).on("error", (err) => {
-  callback(err);
+http.post('apps.ce.collabserv.com/activities/service/atom2/activity?activityUuid=ac7081f8-417c-407c-a3bb-c13ddc541ea8', data, function(res){
+res.setEncoding('utf8');
+	res.on('data', function(chunk) {
+			console.log("************************************Data************************************")
+			console.log(data)		
+	});.
 });
+	
+//http.post(options, (resp) => {
+//  let data = '';
+//
+//  resp.on('data', (chunk) => {
+//    data += chunk;
+//  });
+//  resp.on('end', () => {
+//	var parser = new xml2js.Parser();
+//		parser.parseString(data, function (err, HTTPresult){
+//		console.log("************************************Data************************************")
+//		console.log(data)		
+//		console.log("************************************Result************************************")
+//		console.log(HTTPresult);
+//		var entries = HTTPresult['feed']['entry'];
+//		var titles = ""
+//		for(var index = 0; index < entries.length; ++index){
+//			if(index>0){
+//				titles+= ", "
+//			}
+//			titles += "\n"+entries[index]['title'][0]['_']+"\n";
+//			if(type == "files"){
+//				var URL = entries[index]['link'][0]['$']['href'].replace(/entry/g, 'media')
+//				titles += URL;
+//			}
+//			
+//		}
+//		callback("These are you "+type+": " + titles)
+//	})
+//  });
+//
+//}).on("error", (err) => {
+//  callback(err);
+//});
 }
