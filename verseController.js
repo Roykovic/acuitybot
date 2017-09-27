a7a04fa79cfba183e782a7de14293a988409bd43
Roykovic <rtersluijsen@acuity.nl>
2017-09-27 15:51:50 +0200

    .

--- verseController.js
+++ verseController.js
@@ -30,10 +30,10 @@ exports.getFromIBM = function (type, callback){
 exports.postToIBM = function (callback, name, type){
 	var path;
 	var body;
-	console.log("******************************************************TYPE******************************************************")
-	console.log(type)
 		switch(type) {
     case "communities":
+		console.log("******************************************************TYPE******************************************************")
+	console.log(type)
 		path = "/communities/service/atom/communities/my"
 		body = '<atom:id>https://apps.ce.collabserv.com/communities/service/atom/catalog/my</atom:id><atom:title><![CDATA[My Communities]]></atom:title><atom:author><atom:name>Communities Catalog REST Services</atom:name></atom:author><atom:link href="https://apps.ce.collabserv.com/communities/service/atom/catalog/my" rel="self" type="application/atom+xml"/><atom:updated>2017-09-27T13:19:57.360Z</atom:updated><opensearch:Query role="request" searchTerms="*"/><opensearch:startIndex>0</opensearch:startIndex><opensearch:itemsPerPage>10</opensearch:itemsPerPage><opensearch:totalResults exact="true">1</opensearch:totalResults><atom:entry><atom:id>59cc8b20-b222-4ddd-9c73-4a01e7ea1215</atom:id><atom:title><![CDATA[Stage Rick en Roy]]></atom:title><atom:contributor><snx:userid>203079380</snx:userid><atom:name>Roy Tersluijsen</atom:name></atom:contributor><atom:link href="https://apps.ce.collabserv.com/communities/service/html/communitystart?communityUuid=59cc8b20-b222-4ddd-9c73-4a01e7ea1215" rel="alternate" hreflang="en"/><atom:updated>2017-09-11T09:07:30.427Z</atom:updated><atom:summary type="html"><![CDATA[  ]]></atom:summary><snx:isExternal>false</snx:isExternal><opensearch:relevance>1.0</opensearch:relevance><ibmsc:field id="ATOMAPISOURCE">https://apps.ce.collabserv.com/communities/service/atom/community/instance?communityUuid=59cc8b20-b222-4ddd-9c73-4a01e7ea1215</ibmsc:field><ibmsc:field id="FIELD_LIST_WHEN_PRIVATE">false</ibmsc:field><ibmsc:field id="AccessControlLevel">public</ibmsc:field><ibmsc:field id="isInternalOnly">true</ibmsc:field><ibmsc:field id="FIELD_COMMUNITY_MEMBER_COUNT">4</ibmsc:field><ibmsc:field id="FIELD_IS_TRASHED">false</ibmsc:field><ibmsc:field id="ImageLastMod">1503905485929</ibmsc:field><ibmsc:field id="groupCount">0</ibmsc:field><ibmsc:field id="FIELD_MEMBERS_UPDATE_DATE">1503905535335</ibmsc:field><ibmsc:field id="ContentSourceType">Communities</ibmsc:field><ibmsc:field id="FIELD_UPDATE_FLAG">NONE</ibmsc:field></atom:entry>'
         break;
