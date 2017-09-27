21bb24c000dde8804ded61350c67d47f25406fa5
Roykovic <rtersluijsen@acuity.nl>
2017-09-27 09:36:14 +0200

    .

--- verseController.js
+++ verseController.js
@@ -46,13 +46,30 @@ exports.postActivities = function (callback){
 
 exports.getJSON = function(method, path, type, callback, body){
 	
-var request = require('request');
-var url ='https://requestb.in/1mn0h171'
-request(url, function (error, response, body) {
-  if (!error) {
-    console.log(body);
+var http = require("http");
+var options = {
+  hostname: 'requestb.in',
+  port: 80,
+  path: '1mn0h171',
+  method: 'POST',
+  headers: {
+      'Content-Type': 'application/json',
   }
-});	
+};
+var req = http.request(options, function(res) {
+  console.log('Status: ' + res.statusCode);
+  console.log('Headers: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (body) {
+    console.log('Body: ' + body);
+  });
+});
+req.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
+// write data to request body
+req.write('{"string": "Hello, World"}');
+req.end();
 	
 
 const https = require('https');
