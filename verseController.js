f879ff3ac86ca3ad441e5c7f222813587a80736e
Roykovic <rtersluijsen@acuity.nl>
2017-09-29 12:09:30 +0200

    .

--- verseController.js
+++ verseController.js
@@ -15,6 +15,7 @@ exports.getFromIBM = function (type, callback){
 		var path = "/communities/service/atom/communities/my"
         break;
     case "activities":
+		console.log("*************************AMK*************************")
         var path = "/activities/service/atom2/?includeCommunityActivities=no"
         break;
 	case "files":
@@ -36,7 +37,6 @@ exports.postToIBM = function (callback, name, type, activity){
 		body = '<?xml version="1.0" encoding="UTF-8"?><entry xmlns="http://www.w3.org/2005/Atom" xmlns:app="http://www.w3.org/2007/app" xmlns:snx="http://www.ibm.com/xmlns/prod/sn"><title type="text">'+name+'</title><content type="html"></content><category term="community" scheme="http://www.ibm.com/xmlns/prod/sn/type"></category><snx:communityType>public</snx:communityType></entry>'
         break;
     case "activities":
-	console.log("*************************AMK*************************")
         path = "/activities/service/atom2/activities"
 		body = '<entry xmlns="http://www.w3.org/2005/Atom" xmlns:app="http://www.w3.org/2007/app" xmlns:snx="http://www.ibm.com/xmlns/prod/sn" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:thr="http://purl.org/syndication/thread/1.0"  > <title type="text">'+name+'</title>    <category scheme="http://www.ibm.com/xmlns/prod/sn/type" term="activity" label="Activity"/>    <category scheme="http://www.ibm.com/xmlns/prod/sn/priority" term="1" label="Normal"/>    <content type="html">             </content></entry>'
         break;
