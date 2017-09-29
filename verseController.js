ed0c062d97d68ed04642ad52b786952f87050cbe
Roykovic <rtersluijsen@acuity.nl>
2017-09-29 12:08:06 +0200

    .

--- verseController.js
+++ verseController.js
@@ -36,6 +36,7 @@ exports.postToIBM = function (callback, name, type, activity){
 		body = '<?xml version="1.0" encoding="UTF-8"?><entry xmlns="http://www.w3.org/2005/Atom" xmlns:app="http://www.w3.org/2007/app" xmlns:snx="http://www.ibm.com/xmlns/prod/sn"><title type="text">'+name+'</title><content type="html"></content><category term="community" scheme="http://www.ibm.com/xmlns/prod/sn/type"></category><snx:communityType>public</snx:communityType></entry>'
         break;
     case "activities":
+	console.log("*************************AMK*************************")
         path = "/activities/service/atom2/activities"
 		body = '<entry xmlns="http://www.w3.org/2005/Atom" xmlns:app="http://www.w3.org/2007/app" xmlns:snx="http://www.ibm.com/xmlns/prod/sn" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:thr="http://purl.org/syndication/thread/1.0"  > <title type="text">'+name+'</title>    <category scheme="http://www.ibm.com/xmlns/prod/sn/type" term="activity" label="Activity"/>    <category scheme="http://www.ibm.com/xmlns/prod/sn/priority" term="1" label="Normal"/>    <content type="html">             </content></entry>'
         break;
@@ -102,7 +103,7 @@ request(options, function (error, response, body) {
 	}
 	//Either an error, or a statuscode for an insuccesful request
 	else{
-		callback("Something went wrong, please check if this record doesn't already exist. And if you have the appropriate rights to fulfill this action (" + response.statuscode + ")")
+		callback("Something went wrong, please check if this record exists. And if you have the appropriate rights to fulfill this action (" + response.statusCode + ")")
 	}
 	console.log("**STATUS**")
 	if(error){console.log(error)}
