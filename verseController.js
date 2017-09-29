7692725c40e6d58558a111079af6d033ebccfd1d
Roykovic <rtersluijsen@acuity.nl>
2017-09-29 16:09:07 +0200

    .

--- verseController.js
+++ verseController.js
@@ -44,6 +44,7 @@ exports.postToIBM = function (callback, name, type, activity){
 		body = '<entry xmlns="http://www.w3.org/2005/Atom" xmlns:app="http://www.w3.org/2007/app" xmlns:snx="http://www.ibm.com/xmlns/prod/sn" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:thr="http://purl.org/syndication/thread/1.0"  > <title type="text">'+name+'</title>    <category scheme="http://www.ibm.com/xmlns/prod/sn/type" term="activity" label="Activity"/>    <category scheme="http://www.ibm.com/xmlns/prod/sn/priority" term="1" label="Normal"/>    <content type="html">             </content></entry>'
         break;
 	case "activity nodes":
+		console.log(exports.getActivityId(activity))
 		if(!exports.getActivityId(activity)){
 			return callback("The activity doesn't exist")
 		}
@@ -53,16 +54,6 @@ exports.postToIBM = function (callback, name, type, activity){
 
 	var method = "POST"
 	return	exports.getJSON(method, path,type, function(speech){
-		callback(speech)
-	}, body);
-
-}
-
-exports.postActivityNodes = function (callback, name){
-	var method = "POST"
-	var body = '<entry xmlns="http://www.w3.org/2005/Atom" xmlns:snx="http://www.ibm.com/xmlns/prod/sn"> <title type="text">'+name+'</title> <category scheme="http://www.ibm.com/xmlns/prod/sn/type" term="todo" label="To Do"/> <content type="html">          	&lt;p dir="ltr">TEST&lt;/p>      	  </content> <snx:communityUuid/> </entry>'
-	var path = "/activities/service/atom2/activity?activityUuid=ac7081f8-417c-407c-a3bb-c13ddc541ea8"
-	return	exports.getJSON(method, path,"activity nodes", function(speech){
 		callback(speech)
 	}, body);
 
@@ -134,18 +125,15 @@ exports.getActivityId = function(activityName){
 				console.log("****************************************BODYTHINGY****************************************")
 				var entries = HTTPresult['feed']['entry'];
 				for(var index = 0; index < entries.length; ++index){			
-					console.log(entries[index]['title'][0]['_'])
-					console.log(activityName)
-					console.log(entries[index]['title'][0]['_'] == activityName)
 					if(entries[index]['title'][0]['_'] == activityName){
 						var unformattedId = entries[index]['id'][0];	
 						var parts = unformattedId.split(':')
 						id = parts[parts.length-1]
 					}				
-					console.log(id)
 				}
 			})
 		}
 })
+console.log("id " + id)
 return id;
 }
