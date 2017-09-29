3fd133332ebf4a43ea676b2cd2b122a09f797ec4
Roykovic <rtersluijsen@acuity.nl>
2017-09-29 16:25:16 +0200

    .

--- verseController.js
+++ verseController.js
@@ -32,12 +32,10 @@ exports.getFromIBM = function (type, callback){
 exports.postToIBM = function (callback, name, type, activity){
 	var path;
 	var body;
-				exports.getActivityId(activity, function(activityID){
+	exports.getActivityId(activity, function(activityID){
 			if(!activityID){
 				return callback("The activity doesn't exist")
 			}
-			
-
 		switch(type) {
     case "communities":
 		path = "/communities/service/atom/communities/my"
@@ -48,7 +46,7 @@ exports.postToIBM = function (callback, name, type, activity){
 		body = '<entry xmlns="http://www.w3.org/2005/Atom" xmlns:app="http://www.w3.org/2007/app" xmlns:snx="http://www.ibm.com/xmlns/prod/sn" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:thr="http://purl.org/syndication/thread/1.0"  > <title type="text">'+name+'</title>    <category scheme="http://www.ibm.com/xmlns/prod/sn/type" term="activity" label="Activity"/>    <category scheme="http://www.ibm.com/xmlns/prod/sn/priority" term="1" label="Normal"/>    <content type="html">             </content></entry>'
         break;
 	case "activity nodes":
-		path = "/activities/service/atom2/activity?activityUuid="+exports.getActivityId(activity)
+		path = "/activities/service/atom2/activity?activityUuid="+activityID
 		body = '<entry xmlns="http://www.w3.org/2005/Atom" xmlns:snx="http://www.ibm.com/xmlns/prod/sn"> <title type="text">'+name+'</title> <category scheme="http://www.ibm.com/xmlns/prod/sn/type" term="todo" label="To Do"/> <content type="html">          	&lt;p dir="ltr">TEST&lt;/p>      	  </content> <snx:communityUuid/> </entry>'
 }
 
