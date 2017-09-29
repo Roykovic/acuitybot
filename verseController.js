40562d127a80bf17c2fd4bae2d2ebf0726741602
Roykovic <rtersluijsen@acuity.nl>
2017-09-29 14:56:39 +0200

    .

--- verseController.js
+++ verseController.js
@@ -130,7 +130,7 @@ exports.getActivityId = function(activityName){
 		if (!error && response.statusCode == 200) {
 			parser.parseString(body, function (err, HTTPresult){
 				console.log("****************************************BODYTHINGY****************************************")
-				console.log(HTTPresult[feed][0])
+				console.log(HTTPresult['feed'][0])
 				id = HTTPresult['feed']['entry']['id'];
 			})
 		}
