91096c02246e197ce4fb59f0146002c059e250c3
Roykovic <rtersluijsen@acuity.nl>
2017-10-03 08:57:50 +0200

    .

--- verseController.js
+++ verseController.js
@@ -50,9 +50,11 @@ exports.postToIBM = function (callback, name, type, activity){
 			if(!activityID){
 				return callback("The activity doesn't exist")
 			}
+			else{
 		path = "/activities/service/atom2/activity?activityUuid="+activityID
 		body = '<entry xmlns="http://www.w3.org/2005/Atom" xmlns:snx="http://www.ibm.com/xmlns/prod/sn"> <title type="text">'+name+'</title> <category scheme="http://www.ibm.com/xmlns/prod/sn/type" term="todo" label="To Do"/> <content type="html">          	&lt;p dir="ltr">&lt;/p>      	  </content> <snx:communityUuid/> </entry>'
-}
+			}
+		}
 
 	var method = "POST"
 	return	exports.getJSON(method, path,type, function(speech){
