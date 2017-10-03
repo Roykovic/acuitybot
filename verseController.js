32c0265a23ecec60610cbba50a80448869d1ff95
Roykovic <rtersluijsen@acuity.nl>
2017-10-03 12:06:04 +0200

    .

--- verseController.js
+++ verseController.js
@@ -12,8 +12,6 @@ exports.auth = "";
 
 exports.getFromIBM = function (type, callback){
 	var path;
-		console.log("******************TYPE********************")
-		console.log(type);
 	switch(type) {
     case "communities":
 		var path = "/communities/service/atom/communities/my"
