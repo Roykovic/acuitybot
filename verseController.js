c2c6233a3c45f6c400cf47bd1b2985342d3faf1b
Roykovic <rtersluijsen@acuity.nl>
2017-10-05 13:49:54 +0200

    .

--- verseController.js
+++ verseController.js
@@ -82,8 +82,8 @@ exports.postToIBM = function (callback, name, type, activity){
 
 exports.updateIBM = function (varName, varValue, callback){
 	exports.getIdByName("testtodo",'/activities/service/atom2/todos', function(id){
-			exports.getJSON("GET", '/activities/service/atom2/activitynode?activityNodeUuid='+id, "todo", function(body){
-				exports.getJSON("PUT", '/activities/service/atom2/activitynode', "todo", function(){
+			exports.getJSON("GET", '/activities/service/atom2/activitynode?activityNodeUuid='+id, "updateTodo", function(body){
+				exports.getJSON("PUT", '/activities/service/atom2/activitynode', "updateTodo", function(){
 					console.log("***********************SHITS DONE YO************************")
 				}, body)
 		
@@ -110,10 +110,10 @@ request(options, function (error, response, body) {
 	//No error, and get was succesful
     if (!error && response.statusCode == 200) {
 		console.log(body)
- 	return parser.parseString(body, function (err, HTTPresult){
-		if(HTTPresult['entry']){
+		if(type = "updateTodo"){
 			return callback(body)
 		}
+ 	return parser.parseString(body, function (err, HTTPresult){
 		var entries = HTTPresult['feed']['entry'];
 		if(!entries){
 			return callback()
