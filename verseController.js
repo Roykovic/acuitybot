eb9193e4252db3c0d0852d861bf569044179eafe
Roykovic <rtersluijsen@acuity.nl>
2017-10-05 16:30:03 +0200

    .

--- verseController.js
+++ verseController.js
@@ -83,7 +83,9 @@ exports.postToIBM = function (callback, name, type, activity){
 exports.updateIBM = function (varName, varValue, callback){
 	exports.getIdByName("testtodo",'/activities/service/atom2/todos', function(id){
 			exports.getJSON("GET", '/activities/service/atom2/activitynode?activityNodeUuid='+id, "updateTodo", function(body){
-				console.log(typeof body);
+			var splittedString = body.split("<entry>")
+			splittedString[1] += '<category scheme="http://www.ibm.com/xmlns/prod/sn/flags" term="completed" label="Completed"/>'
+			body = splittedString.join();
 				exports.getJSON("PUT", '/activities/service/atom2/activitynode?activityNodeUuid='+id, "updateTodo", function(parameter){
 				}, body)
 		
