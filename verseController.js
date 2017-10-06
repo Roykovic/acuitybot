150c95eda3a9255f46990f6f6aaa10dc26488597
Roykovic <rtersluijsen@acuity.nl>
2017-10-06 12:08:03 +0200

    .

--- verseController.js
+++ verseController.js
@@ -87,7 +87,7 @@ exports.updateIBM = function (varName, varValue, callback){
 				var splittedString = body.split('</entry>')
 				var completed = '<category scheme="http://www.ibm.com/xmlns/prod/sn/flags" term="completed" label="Completed"/>'
 				body = splittedString[0] + completed + splittedString[1]
-				for ( property in splittedString ) {
+				for ( key in splittedString ) {
 					console.log( property ); // Outputs: foo, fiz or fiz, foo
 				}
 				exports.getJSON("PUT", '/activities/service/atom2/activitynode?activityNodeUuid='+id, "updateTodo", function(parameter){
