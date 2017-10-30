caaa01076115bb93bcef9ea13e8f4338dbe20e2a
Roykovic <roykovic97@outlook.com>
2017-10-30 10:13:35 +0100

    .

--- index.js
+++ index.js
@@ -165,7 +165,7 @@ function wakeUp() {
 
 function log(reqIn, resIn, callback){
 	var fs = require('fs');
-	fs.writeFile("logs.csv", "Hey there!", function(err) {
+	fs.writeFile("/files", "Hey there!", function(err) {
 		if(err) {
 			return console.log(err);
 		}
