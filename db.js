12327248079d3bbbaebcc366fafbd3b8b683f7d1
Roykovic <roykovic97@outlook.com>
2017-11-02 09:16:13 +0100

    .

--- db.js
+++ db.js
@@ -92,6 +92,6 @@ exports.log = function(reqIn, resIn, score, intent,callback){
 	  con.query('INSERT INTO logs (request, result, score, intent) VALUES (?,?,?,?)', [reqIn, resIn, score, intent],  function (err, result) {
 		if (err) throw err;
 	  });
-	  callback(con.end();)
+	  callback(con.end())
 	});	
 }
