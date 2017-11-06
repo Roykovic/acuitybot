f2d333936df4396f1ba97e30b5759bf46907cc21
Roykovic <roykovic97@outlook.com>
2017-11-06 10:46:31 +0100

    .

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ restService.get('/connections', function(req, res) {
 
 restService.get('/auth', function(req, res) {
 	var headers = {
-        "code": req.query("code"),
+        "code": req.query.code,
 		"grant_type": "authorization_code",
 		"client_id": "3MVG9HxRZv05HarTorx5Mf0IjDgnpJGwNuO0DCiL0y070i3yFQiLuVegdzZ9oupv5F2AWU1rRT5fv9EpGGfb1",
 		"client_secret": "236296553525088968",
