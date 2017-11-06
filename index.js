2ed9b646a1d107c9b7de241c5e70ecfe398f979b
Roykovic <roykovic97@outlook.com>
2017-11-06 14:04:23 +0100

    .

--- index.js
+++ index.js
@@ -70,10 +70,9 @@ restService.get('/auth', function(req, res) {
 
     // Start the request
     httpRequest(options, function(error, response, body) {
-		console.log(body)
+		console.log(body.keys)
 		body = JSON.parse(body)
-		console.log(body.access_token)
-		console.log(body.refresh_token)
+		//console.log(body.access_token)
 		res.sendFile(__dirname + '/OAuth/loginSucces.html');
 	})
 })
