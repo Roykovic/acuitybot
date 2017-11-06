c6d4b979eb4e32859c61bd973b3b8294fbf71c22
Roykovic <roykovic97@outlook.com>
2017-11-06 09:42:04 +0100

    .

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.get('/connections', function(req, res) {
-	res.sendFile('./index.html');
+	res.sendFile(__dirname + '/index.html');
 })
 
 restService.post('/hook', function(req, res) {
