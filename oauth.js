7440a78124fa84a6df84ed344494207e8f6cf1bf
Roykovic <roykovic97@outlook.com>
2017-11-10 10:39:33 +0100

    .

--- oauth.js
+++ oauth.js
@@ -40,7 +40,7 @@ exports.getTokens = function(service, code, userID){
     httpRequest(options, function(error, response, body) {
 		body = JSON.parse(body)
 		console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-		console.log(body.access_token)
+		console.log(error + response)
 		var access_token = body.access_token
 		return exports.registerToken(userID, access_token, function(access_token){
 			return access_token
