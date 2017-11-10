a8e40d0eda3f029ef7b70e4f1cd6e62a49a81601
Roykovic <roykovic97@outlook.com>
2017-11-10 15:52:40 +0100

    .

--- salesforceController.js
+++ salesforceController.js
@@ -67,7 +67,7 @@ exports.updateUserInfo = function(userID, fullname, column, variable, callBack){
 					'url': 'https://eu11.salesforce.com/'+url,
 					'method': "PATCH",
 					'headers': headers,
-					'body': body
+					'form': body
 				}				
 				
 				httpRequest(options, function(error, response, body) {
