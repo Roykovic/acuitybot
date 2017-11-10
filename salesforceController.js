20c4949a0dbb30b15f008a19cbabf771a7df8780
Roykovic <roykovic97@outlook.com>
2017-11-10 12:07:49 +0100

    .

--- salesforceController.js
+++ salesforceController.js
@@ -78,8 +78,8 @@ exports.getUserInfo = function(userID, fullname, column, callBack){
 		exports.getContacts(access_token, function(contacts){
 			for (var i = 0, len = contacts.length; i < len; i++) {
 				if(contacts[i].Name == fullname){
-					var column = column.charAt(0).toUpperCase() + column.slice(1);
-					var speech = fullname+"'s " + column + " is " + contacts[i].column
+					var capColumn = column.charAt(0).toUpperCase() + column.slice(1);
+					var speech = fullname+"'s " + column + " is " + contacts[i].capColumn
 					callBack(speech)
 				}
 			}
