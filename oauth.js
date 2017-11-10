4713e8ace3503099fc1652b0837013be9bf726ac
Roykovic <roykovic97@outlook.com>
2017-11-10 15:37:29 +0100

    .

--- oauth.js
+++ oauth.js
@@ -48,7 +48,6 @@ exports.getTokens = function(service, code, userID){
 
 exports.registerToken = function(userID, access_token, callback){
 	return db.query('INSERT INTO auth (userID, access_token) VALUES (?,?)', [userID, access_token], function(result){
-		console.log(result)
 		return callback()
 	})
 }
