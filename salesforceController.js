76b3f6493a986fbe1809e08c26b5f788d498a905
Roykovic <roykovic97@outlook.com>
2017-11-10 11:59:44 +0100

    .

--- salesforceController.js
+++ salesforceController.js
@@ -76,12 +76,13 @@ exports.updateQuery = function(column, variables, callBack){
 exports.getUserInfo = function(userID, fullname, column, callBack){
 	oauth.getAccessToken(userID, function(access_token){
 		exports.getContacts(access_token, function(contacts){
-			//for (var i = 0, len = contacts.length; i < len; i++) {
-			console.log(contacts[i].Name)
-			console.log(fullname)
+			for (var i = 0, len = contacts.length; i < len; i++) {
+				console.log(contacts[i].Name)
+				console.log(fullname)
 				if(contacts[i].Name == fullname){
 					console.log("fullname")
 				}
+			}
 		})
 	})
 }
