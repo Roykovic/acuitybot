c1fb2bcb27522433f06574534f83dc6c912b5e53
Roykovic <roykovic97@outlook.com>
2017-11-10 10:14:49 +0100

    .

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ restService.get('/login/:service/:userID', function(req, res) {
 })
 
 restService.get('/auth/:service', function(req, res) {
-	console.log(req.rawHeaders.id_token);
+	console.log(req.rawHeaders);
     OAuthController.getTokens(req.params.service, req.query.code, userID)
 	res.sendFile(__dirname + '/OAuth/loginSucces.html');
 })
