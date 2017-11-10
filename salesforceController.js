22c6186430204d375f202a51895ba376a9737506
Roykovic <roykovic97@outlook.com>
2017-11-10 12:11:13 +0100

    .

--- salesforceController.js
+++ salesforceController.js
@@ -79,7 +79,7 @@ exports.getUserInfo = function(userID, fullname, column, callBack){
 			for (var i = 0, len = contacts.length; i < len; i++) {
 				if(contacts[i].Name == fullname){
 					column = column.charAt(0).toUpperCase() + column.slice(1);
-					var speech = fullname+"'s " + column + " is " + contacts[i].column
+					var speech = fullname+"'s " + column + " is " + contacts[i].Email
 					callBack(speech)
 				}
 			}
