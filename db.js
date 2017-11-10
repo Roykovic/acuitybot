22791b68a09a868d100b9029100206fa582e8abb
Roykovic <roykovic97@outlook.com>
2017-11-10 09:51:57 +0100

    .

--- db.js
+++ db.js
@@ -112,7 +112,7 @@ exports.log = function(reqIn, resIn, score, intent,callback){
    pool.getConnection(function(err,connection){
         if (err) {
 			console.log("ERROR " + err)
-				connection.end();
+				connection.release();
           callback();
           return;
         }
@@ -120,13 +120,13 @@ exports.log = function(reqIn, resIn, score, intent,callback){
             connection.release();
             if(!err) {
 				console.log("SUCCESFUL")
-					connection.end();
+					connection.release();
                 callback();
             }
         });
         connection.on('error', function(err) {
 			console.log("CONNECTION ERROR")
-				connection.end();
+				connection.release();
               callback();
               return;
         });
