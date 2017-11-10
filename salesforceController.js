86ce25dc0a172665dfd118f395edf75cdffec0ae
Roykovic <roykovic97@outlook.com>
2017-11-10 13:44:20 +0100

    .

--- salesforceController.js
+++ salesforceController.js
@@ -60,7 +60,7 @@ exports.getIdByName = function(userID, fullname, callBack){
 		exports.getContacts(access_token, function(contacts){
 			for (var i = 0, len = contacts.length; i < len; i++) {
 				if(contacts[i].Name == fullname){
-					console.log(contact[i].id)
+					console.log(contacts[i].id)
 					console.log(Object.keys(contact[i]))
 					//return callback(var id = contact[i].id)
 				}
