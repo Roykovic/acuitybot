f05696f79d6bd8e4075d25339fb2a03de95a0580
Roykovic <roykovic97@outlook.com>
2017-11-10 14:44:26 +0100

    .

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ restService.post('/hook', function(req, res) {
             var context = req.body.result
             var column = context.parameters.Variable_row;
             var variable = context.parameters['variable']
-			var fullname = context.parameters['sf-name']
+			var fullname = context.parameters['fullName']
 			salesforceController.updateUserInfo(userID, fullname, column, variable, function(){
 				return returnJson(context.parameters['sf-name'] + "\'s " + column + " changed to " + context.parameters['variable']);
 			})
