97cd0d2aa1a5dca9917746eb7e42868b67628156
Roykovic <roykovic97@outlook.com>
2017-11-13 08:48:28 +0100

    .

--- salesforceController.js
+++ salesforceController.js
@@ -36,7 +36,7 @@ exports.getColumns = function(userID, callback){
 		
 		httpRequest(options, function(error, response, body) {
 			body = JSON.parse(body)
-			console.log(response)
+			console.log(error)
 			for (var i = 0, len = body.fields.length; i < len; i++) {
 				console.log(body.fields[i].name)
 			}			
