fdadcb6b4a248155eab3580537ec74d6c35d4df0
Roykovic <roykovic97@outlook.com>
2017-11-15 11:57:48 +0100

    .

--- index.js
+++ index.js
@@ -179,7 +179,7 @@ function log(reqIn, resIn, score, intent, callback){
 function returnJson(speech, followUp) {
 	var postPath = "https://api.api.ai/v1/userEntities?v=20150910&sessionId=" + sessionId
 	var accesToken = "5462b4a0987946ee967dbea809dd6676";
-	var body = '{ "entities": [ { "entries": [ { "value": "Gradje hendriks" }, ], "name": "sf-name" } ], "sessionId":' +sessionId+ '}'
+	var body = '{ "entities": [ { "entries": [ {           "synonyms": ["Gradje Hendriks"],"value": "Gradje Hendriks" }, ], "name": "sf-name" } ], "sessionId":' +sessionId+ '}'
 	
 	return apiController.post(postPath, accesToken, body, function(dingen){
 		console.log("**********************************************************************")
