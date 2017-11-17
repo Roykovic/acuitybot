59d91300136ac2cbcf89b980330eb6b73b867fe9
Roykovic <roykovic97@outlook.com>
2017-11-17 15:06:31 +0100

    .

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ restService.get('/login/:service/:userID/:sessionId', function(req, res) {
 restService.get('/auth/:service', function(req, res) {
 	var userID = req.cookies.id_token;
 	var sessionId = req.cookies.session_token;
+	console.log(sessionId)
     OAuthController.getTokens(req.params.service, req.query.code, userID)
 	userController.addUserEntities(sessionId, userID, function(succes){
 	res.sendFile(__dirname + '/OAuth/loginSucces.html');})
