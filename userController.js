59105dfc953ad2a9185838ec50764089360db9d3
Roykovic <roykovic97@outlook.com>
2017-11-17 09:01:47 +0100

    .

--- userController.js
+++ userController.js
@@ -81,7 +81,7 @@ exports.addUserEntities = function(sessionId,userId, callback){
 	var accesToken = "5462b4a0987946ee967dbea809dd6676";
 	
 	apiController.get(getPath,function(body){
-		console.log(body.entries)
+		console.log(body)
 		if(body.entries < 0){
 			var body = '{ "entities": [ { "entries": ['
 			var bodyEnd = '], "name": "sf-name" } ], "sessionId":' +sessionId+ '}'
