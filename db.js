00450e5d106ff13dca2449295b07c18196d3bc4f
Roykovic <roykovic97@outlook.com>
2017-11-17 14:48:08 +0100

    .

--- db.js
+++ db.js
@@ -16,7 +16,7 @@ exports.query = function (query, params, callback){
         }
         connection.query(query, params,function(err,results){
             if(!err) {
-				console.log("query2")
+				console.log(results)
 				connection.release();
                 callback(results, true);
 				return
