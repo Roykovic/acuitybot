5c3b497814ff5cc8298e0272715578ede6ab3a43
Roykovic <roykovic97@outlook.com>
2017-11-21 13:17:27 +0100

    .

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ restService.get('/auth/:service', function(req, res) {
 })
 
 restService.post('/hook', function(req, res) {
-    console.log(sessionId)
+    console.log("Session ID : " + sessionId)
     userID = req.body.originalRequest.data.data.personId
     request = req;
     result = res;
