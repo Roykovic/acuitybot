80eeea73615bc80ef51073b83a97290e530ccd9a
Roykovic <roykovic97@outlook.com>
2017-11-22 14:22:13 +0100

    .

--- userController.js
+++ userController.js
@@ -74,6 +74,12 @@ exports.getServiceByName = function(fullname, userID, callback) {
 }
 
 exports.addUserEntities = function(sessionId, userId, callback) {
+ 	
+    console.log("caller is " + arguments.callee.caller.toString())
+	console.log("****************************SPEECH********************************")
+	console.log(speech)
+	
+	
     var postPath = "https://api.api.ai/v1/userEntities?v=20150910&sessionId=" + sessionId
 
     exports.getUserEntities(sessionId, function(response) {
