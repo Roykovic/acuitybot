a02133760713ecd255c70673a37cf1761f22d7c7
Roykovic <roykovic97@outlook.com>
2017-11-24 10:59:46 +0100

    .

--- utils/httpUtils.js
+++ utils/httpUtils.js
@@ -4,5 +4,11 @@ var exports = module.exports = {};
 
 exports.parseFormData = function(formData){
 	var splitString = formData.split("&")
-	console.log(splitString);
+	var returnBody = {};
+	
+	for(var i = 0; i<splitString.length; ++i){
+		var element = splitString[i].split("=")
+		returnBody[element[0]] = element[1]
+	}
+	console.log(returnBody);
 }
