d7186bf1bee1f56a7e6d9e6ada0b0f057459f572
Roykovic <roykovic97@outlook.com>
2017-11-24 11:45:09 +0100

    .

--- oauth.js
+++ oauth.js
@@ -59,7 +59,7 @@ exports.registerToken = function(service, userID, access_token, expiresAt, callb
 }
 
 exports.getAccessToken = function(service, userID, callback) {
-    return db.query('SELECT 'service'_access_token, 'service'_expires_at FROM auth WHERE userID = ?', userID, function(result) {
+    return db.query('SELECT '+service+'_access_token, '+service+'_expires_at FROM auth WHERE userID = ?', userID, function(result) {
         if (result && result.length > 0) {
             if (result[0].expires_at < new Date()) {
                 return callback()
