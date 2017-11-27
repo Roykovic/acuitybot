68564f5de6561ea9ef086aab4b42dc505eb5cd36
Roykovic <roykovic97@outlook.com>
2017-11-27 14:52:34 +0100

    .

--- oauth.js
+++ oauth.js
@@ -89,8 +89,7 @@ exports.checkExpiration = function(userID, callback){
 		return db.query(query, userID, function(result){
 			i++;
 			serviceResult = result[0];
-			console.log(service+"_expires_at")
-			if (result.length < 1 || serviceResult[service+"_expires_at"] < new Date()) {
+			if (result.length < 1 || serviceResult['ibm'+"_expires_at"] < new Date()) {
 				if(serviceResult[service+"_refresh_token"]){
 					exports.refreshAccesToken(serviceResult[service, service+"_refresh_token"])
 				}
