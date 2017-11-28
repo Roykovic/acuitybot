e6ce332d6c78a9302d47e4ddb824c845d5d1c226
Roykovic <roykovic97@outlook.com>
2017-11-28 13:30:03 +0100

    .

--- ibmController.js
+++ ibmController.js
@@ -235,6 +235,7 @@ exports.getContacts = function(access_token, callback) {
 }
 
 exports.getUserInfo = function(userID, fullname, column, callback) {
+	column = column.toLowerCase()
 	if(Object.keys(config.ibm.columns).indexOf(column) > 1){
 		console.log("FOUND A NIFFO")
 	}
@@ -253,7 +254,7 @@ exports.getUserInfo = function(userID, fullname, column, callback) {
 			if (!error && response.statusCode == 200) {
 				return parser.parseString(body, function(err, HTTPresult) {
 					var entries = HTTPresult['feed']['entry'];
-					var answer = entries[0]['contributor'][0][column.toLowerCase()][0]
+					var answer = entries[0]['contributor'][0][column][0]
 					return callback(fullname + ' \'s ' + column + ' is ' + answer)
 					return callback(fullname + ' \'s ' + column + ' could not be found');
 				})
