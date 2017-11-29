8ee26c349fc6313ebafeb99b1794fe349003ee04
Roykovic <roykovic97@outlook.com>
2017-11-29 10:03:37 +0100

    .

--- ibmController.js
+++ ibmController.js
@@ -252,7 +252,7 @@ exports.getUserInfo = function(userID, fullname, column, callback) {
 					return parser.parseString(body, function(err, json) {
 						var matches = xpath.find(json, "//span", "class");
 						console.log("MATCHES")
-						console.log(matches[1][5])
+						console.log(Object.keys(matches))
 						var entries = json['feed']['entry'];
 						var answer = entries[0]['contributor'][0][column][0]
 						return callback(fullname + '\'s ' + column + ' is ' + answer)
