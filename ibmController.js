21b3eb0f21f549900ba47c158d50dda1d61ef528
Roykovic <roykovic97@outlook.com>
2017-11-30 14:13:39 +0100

    .

--- ibmController.js
+++ ibmController.js
@@ -14,7 +14,7 @@ var parser = new xml2js.Parser();
 exports.getFromIBM = function(userID, type, callback) {
     var path;
     switch (type) {
-        case "communities":
+        case "communiies":
             var path = "communities/service/atom/communities/my"
             break;
         case "activities":
