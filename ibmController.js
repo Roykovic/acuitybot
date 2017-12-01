5698eeb13809293b33c6c4d95e2688c61549df5b
Roykovic <roykovic97@outlook.com>
2017-12-01 13:37:33 +0100

    .

--- ibmController.js
+++ ibmController.js
@@ -165,7 +165,7 @@ exports.getJSON = function(access_token, method, path, type, callback, body) {
 
 exports.getIdByName = function(access_token, varName, path, callback) {
 	console.log("VARNAME")
-	console.log(varname)
+	console.log(varName)
     var id = "";
     var headers = {
         "Content-Type": 'application/atom+xml',
