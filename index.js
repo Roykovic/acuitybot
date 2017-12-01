4f253be33643629eec13a3b4a04baa02f12ce364
Roykovic <roykovic97@outlook.com>
2017-12-01 09:37:19 +0100

    .

--- index.js
+++ index.js
@@ -68,6 +68,7 @@ restService.post('/hook', function(req, res) {
         case "data for update":
 			var parameters = req.body.result.parameters
 			if(!parameters) parameters = req.body.result.contexts[1].parameters
+			console.log(parameters)
             var column = parameters.Variable_row;
             var variable = parameters['variable.original'].replace("?","")
             var fullname = parameters['sf-name']
