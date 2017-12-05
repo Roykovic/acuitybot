dc03c404b29f27687c355b4a942a8f7aeba4f932
Roykovic <roykovic97@outlook.com>
2017-12-05 09:14:58 +0100

    .

--- index.js
+++ index.js
@@ -107,6 +107,8 @@ restService.post('/hook', function(req, res) {
                 })
             }
             return userController.getServiceByName(fullName, userID, function(serviceType) {
+				console.log("SERVICETYPE")
+				console.log(serviceType)
                 if (serviceType == service.services.IBM) {
 					return ibmController.getUserInfo(userID, fullName, column, function(speech, followUp) {
                         return returnJson(res, req, speech, followUp)
